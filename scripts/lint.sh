b429d1fb2febba324b01f5f33f1dd3e46250b6f2
Holden Omans <holden.omans@gmail.com>
2021-06-13 09:34:13 -0400

    minor update

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-docker run --rm -ti -v $(pwd):/app -w /app/charts/ quay.io/helmpack/chart-testing /bin/sh -c "ct lint --chart-dirs /app/charts/ --all "
+docker run --rm -ti -v $(pwd):/app -w /app/charts/ quay.io/helmpack/chart-testing /bin/sh -c "ct lint --chart-dirs /app/charts/ --all --config /app/ct.yaml"
